a5f290f3ffa01665b05cb48664d9434958a2c0e2
MafaldaNogueira <mafaldanogueiraapm@hotmail.com>
2020-11-24 09:37:43 +0000

    styles

--- src/components/header.js
+++ src/components/header.js
@@ -4,9 +4,16 @@ function SayHello() {
     return (
         <React.Fragment>
         <div className="headerImage">
-        <h1>Say hello to ReactJS</h1>
-        <p> You will learn how to use the most popular frontend <br/>
-        library and become a super Ninja developer. </p>
+        <div className="headerTop">
+        <img src="../../images/ironhack-logo.svg"/>
+        <img src="../../images/menu-top.svg"/>
+        </div>
+        <h1>Say hello to <br/>
+        ReactJS</h1>
+        <p> You will learn how to use <br/>
+
+        the most popular frontend library <br/>
+        and become a super Ninja developer. </p>
         <button> Awesome!</button>
         </div>
         </React.Fragment>
